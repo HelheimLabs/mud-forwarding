a451fd84cb76852b551d5e4d4d046cc2683c7748
Sanbao Zheng <baobaobao369@163.com>
2023-10-30 14:15:33 +0800

    filter out unregistered player

--- packages/contracts/src/systems/CoreSystem.sol
+++ packages/contracts/src/systems/CoreSystem.sol
@@ -18,7 +18,7 @@ contract CoreSystem is System {
     address player = _msgSender();
     GameEnvData memory env = GameEnv.get();
     PlayerData memory data = Player.get(player);
-    require(block.number > data.lastUpdate + env.actionPeriod, "last game not ended");
+    require(data.lastUpdate == 0 || block.number > data.lastUpdate + env.actionPeriod, "last game not ended");
     _checkAndRecordScore(player);
     Player.set(player, PlayerData({
       level: 0,
@@ -69,6 +69,16 @@ contract CoreSystem is System {
     uint16 playerId = PlayerInfo.getUuid(player);
     uint16 robberId = uint16(data.robber >> 16);
     uint16 robberValue = uint16(data.robber);
+    // if a player has not yet registered
+    if (playerId == 0) {
+      if (data.value > robberValue) {
+        Player.setValue(player, data.value - robberValue);
+        Player.setRobber(player, 0);
+      } else {
+        Player.deleteRecord(player);
+      }
+      return;
+    }
     if (robberId == playerId) {
       Player.setValue(player, data.value + robberValue);
       Player.setRobber(player, 0);
@@ -134,7 +144,7 @@ contract CoreSystem is System {
   function _checkAndRecordScore(address player) internal {
     PlayerData memory data = Player.get(player);
     if (data.level > 0) {
-      _recordScore(player, data.level, data.value);
+      _recordScore(player, data.level, 0);
     }
   }
 
